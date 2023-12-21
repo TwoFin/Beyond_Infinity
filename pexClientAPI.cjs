15f31ad3e459119dfda144563765c370fd149d01
Jon Turner <jon.turner@hotmail.co.uk>
2023-12-21 20:01:26 +1100

    Handle wrong Class level

--- pexClientAPI.cjs
+++ pexClientAPI.cjs
@@ -8,6 +8,9 @@ const pexnodeapi = "https://" + pexnode + "/api/client/v2/conferences/";
 
 class controlClass {
   async lowerClass(vmr, level) {
+    console.log(
+      "CLIENT_API: Recieved level change request for: " + vmr + " to:" + level
+    );
     // Obtain token
     var url = pexnodeapi + vmr + "/request_token";
     var json = { display_name: clientapi_name, call_tag: clientapi_tag };
@@ -30,7 +33,7 @@ class controlClass {
     var data = await response.json();
     var current_level = data.result.current;
     var level_map = data.result.levels;
-    console.log("CLIENT_API: Availible security level are: ", level_map);
+    console.log("CLIENT_API: Availible security levels are: ", level_map);
     console.log("CLIENT_API: Current security level is: ", current_level);
 
     // Reverse look up new participant classification level from level_map
@@ -44,16 +47,39 @@ class controlClass {
       level
     );
 
-    // Lower clissifiction level if new participant has lower class
-    if (new_level < current_level) {
-      var url = pexnodeapi + vmr + "/set_classification_level";
-      var json = { level: new_level };
-      console.log(
-        "CLIENT_API: Requesting level change to",
-        json.level,
-        "from:",
-        url
+    // Check if new_level has resolved
+    if (new_level) {
+      // Lower clissifiction level if new participant has lower class
+      if (new_level < current_level) {
+        var url = pexnodeapi + vmr + "/set_classification_level";
+        var json = { level: new_level };
+        console.log(
+          "CLIENT_API: Requesting level change to",
+          json.level,
+          "from:",
+          url
+        );
+        var response = await fetch(url, {
+          method: "post",
+          body: JSON.stringify(json),
+          headers: { "Content-Type": "application/json", token: thistoken },
+        });
+        var data = await response.json();
+        console.log("CLIENT_API:", response.status);
+      } else {
+        console.log(
+          "CLIENT_API: No level change required as new participant has required clearance"
+        );
+        new_level = current_level;
+      }
+    }
+    // Set to 0 level as participant does not have a matching classification
+    else {
+      console.warn(
+        "Participant does not have matching classification setting, changing VMR to zero level"
       );
+      var url = pexnodeapi + vmr + "/set_classification_level";
+      var json = { level: 0 };
       var response = await fetch(url, {
         method: "post",
         body: JSON.stringify(json),
@@ -61,11 +87,6 @@ class controlClass {
       });
       var data = await response.json();
       console.log("CLIENT_API:", response.status);
-    } else {
-      console.log(
-        "CLIENT_API: No level chenge required as new participant has required clearance"
-      );
-      new_level = current_level;
     }
 
     // Release Token to disocnnect
@@ -76,10 +97,6 @@ class controlClass {
       headers: { token: thistoken },
     });
     console.log("CLIENT_API:", response.status);
-
-    //  Log resulting level
-    const result = level_map[new_level];
-    console.log("CLIENT_API: New classification level: ", result);
   }
 }
 module.exports = controlClass;
