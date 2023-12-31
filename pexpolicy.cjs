9c64e078652e44330b9096504824090b943f089f
Jon Turner <jon.turner@hotmail.co.uk>
2023-12-31 16:38:52 +1100

    Some formatting change

--- pexpolicy.cjs
+++ pexpolicy.cjs
@@ -6,7 +6,6 @@ const clientAPI = require("./pexClientAPIv2.cjs");
 const clientapi_name = process.env.PEXIP_CLIENTAPI_NAME;
 const clientapi_tag = process.env.PEXIP_CLIENTAPI_TAG;
 
-
 // configuration file
 const pexpolicyConfig = require("./pexpolicyConfig.json");
 // Set lists for IDP processing from configuration file
@@ -43,10 +42,7 @@ class PexPolicy {
         service_type: "conference",
         host_identity_provider_group: "",
       };
-      console.info(
-        "PEXPOLICY: MEETBOT bypassing service with no IDP",
-        pol_response
-      );
+      console.info("PEXPOLICY: MEETBOT bypassing service with no IDP", pol_response);
       return new Promise((resolve, _) => resolve(pol_response));
     }
 
@@ -78,67 +74,44 @@ class PexPolicy {
       query.protocol === "api"
     ) {
       pol_response.result = {
-        remote_display_name:
-          query.idp_attribute_jobtitle +
-          " " +
-          query.idp_attribute_surname +
-          " | " +
-          query.idp_attribute_department,
+        remote_display_name: query.idp_attribute_jobtitle + " " + query.idp_attribute_surname + " | " + query.idp_attribute_department,
       };
-      console.info(
-        "PEXPOLICY: Display name updated: ",
-        pol_response.result.remote_display_name
-      );
+      console.info("PEXPOLICY: Display name updated: ", pol_response.result.remote_display_name);
     } else {
-      console.info(
-        "PEXPOLICY: Skipping build overlay text name, default will be used"
-      );
+      console.info("PEXPOLICY: Skipping build overlay text name, default will be used");
     }
 
     // Extract params from service_tag
     const tag_params = query.service_tag.split("_");
     console.info("PEXPOLICY: service_tag parmameters: ", tag_params);
 
-    // All departments tag - continue based on VMR config - allows classification change based on idp_attribute_clearance
     switch (true) {
-      case (tag_params[0] === "allDept") : {
+      // All departments tag - continue based on VMR config - allows classification change based on idp_attribute_clearance
+      case tag_params[0] === "allDept": {
         // Only do ClientAPI call if protocol is API - prevents double handle
         if (query.protocol === "api") {
-          console.info(
-            "PEXPOLICY: Using ClientAPI to change VMR classification level to",
-            query.idp_attribute_clearance
-          );
-          
-          clientAPI.lowerClassLevel(
-            query.service_name,
-            query.idp_attribute_clearance
-          );
+          console.info("PEXPOLICY: Using ClientAPI to change VMR classification level to", query.idp_attribute_clearance);
+
+          clientAPI.lowerClassLevel(query.service_name, query.idp_attribute_clearance);
         }
         console.info("PEXPOLICY: Participant policy done:", pol_response);
         return new Promise((resolve, _) => resolve(pol_response));
       }
 
       // Entry condition based on rank
-      case (tag_params[0] === "rank") : {
-        if (
-          tag_params[1] === "co" &&
-          rankCo.includes(query.idp_attribute_jobtitle)
-        ) {
+      case tag_params[0] === "rank": {
+        if (tag_params[1] === "co" && rankCo.includes(query.idp_attribute_jobtitle)) {
           // CO Memeber
           console.info("PEXPOLICY: Participants idp jobtitle is on CO list OK");
           console.info("PEXPOLICY: Participant policy done:", pol_response);
           return new Promise((resolve, _) => resolve(pol_response));
-        } else if (
-          tag_params[1] === "top" &&
-          rankTop.includes(query.idp_attribute_jobtitle)
-        ) {
+        } else if (tag_params[1] === "top" && rankTop.includes(query.idp_attribute_jobtitle)) {
           // Top Member
           console.info("PEXPOLICY: Participants idp jobtitle is on TOP list OK");
           console.info("PEXPOLICY: Participant policy done:", pol_response);
           return new Promise((resolve, _) => resolve(pol_response));
         } else {
-          pol_response_reject.result.reject_reason =
-            "ACCESS DENIED You do not have the required rank";
+          pol_response_reject.result.reject_reason = "ACCESS DENIED You do not have the required rank";
           console.info("PEXPOLICY: Participants idp jobtitle NOT in any rank list");
           console.info("PEXPOLICY: Participant policy done:", pol_response_reject);
           return new Promise((resolve, _) => resolve(pol_response_reject));
@@ -146,37 +119,29 @@ class PexPolicy {
       }
 
       // Entry condition based on idp attribute from idpAttr list
-      case (idpAttrs.includes(tag_params[0])) : {
+      case idpAttrs.includes(tag_params[0]): {
         // Extract idp attribute to check
         const idpCheckAttr = "idp_attribute_" + tag_params[0];
 
         // Admit participant if idp attribute matches 2nd tag parameter
         if (query[idpCheckAttr] === tag_params[1]) {
-          console.info(
-            "PEXPOLICY: Participants idp attribute matches service_tag OK"
-          );
+          console.info("PEXPOLICY: Participants idp attribute matches service_tag OK");
           console.info("PEXPOLICY: Participant policy done:", pol_response);
           return new Promise((resolve, _) => resolve(pol_response));
         }
 
         // Reject if no match
         else {
-          pol_response_reject.result.reject_reason =
-            "ACCESS DENIED You are not in the " + tag_params[1];
-          console.info(
-            "PEXPOLICY: Participants idp attribute does NOT match service_tag"
-          );
+          pol_response_reject.result.reject_reason = "ACCESS DENIED You are not in the " + tag_params[1];
+          console.info("PEXPOLICY: Participants idp attribute does NOT match service_tag");
           console.info("PEXPOLICY: Participant policy done:", pol_response_reject);
           return new Promise((resolve, _) => resolve(pol_response_reject));
         }
       }
 
       // Default response
-      default : {
-        console.info(
-          "PEXPOLICY: Participant policy done, default response:",
-          pol_response
-        );
+      default: {
+        console.info("PEXPOLICY: Participant policy done, default response:", pol_response);
         return new Promise((resolve, _) => resolve(pol_response));
       }
     }
